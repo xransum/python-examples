530312f6ba50f72b889b475c46b8939508027dd1
xransum <xransum@users.noreply.github.com>
2024-11-11 18:39:36 -0500

    Updated create_new.py.

--- create_new.py
+++ create_new.py
@@ -5,15 +5,16 @@ import os
 import re
 from glob import glob
 
+DIR_REGEX = r"^[0-9]+_"
+
 root = os.path.dirname(os.path.abspath(__file__))
-dirs = [d for d in os.listdir(root) if os.path.isdir(d)]
+dirs = [d for d in os.listdir(root) if os.path.isdir(d) and re.search(DIR_REGEX, d) is not None]
 arg = input("Input: ").strip()
 new_dir_name = "_".join(re.findall(r"\w+", arg.strip().lower()))
 new_dir_path = os.path.join(root, f"{str(len(dirs) + 1).rjust(2, '0')}_{new_dir_name}")
 
-
 for dir_name in dirs:
-    name = re.sub(r"^[0-9]+_", "", dir_name)
+    name = re.sub(DIR_REGEX, "", dir_name)
     if name == new_dir_name:
         print(f"Dir name exists: {new_dir_name} under {dir_name}.")
         sys.exit(1)
