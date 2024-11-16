22a089badff79f1e2afa48c08af726363955c2ff
xransum <xransum@users.noreply.github.com>
2024-11-16 18:27:34 -0500

    Added Calculate the multiplication and sum of two numbers.

---
+++ section_b/01_calculate_the_multiplication_and_sum_of_two_numbers/main.py
@@ -0,0 +1,12 @@
+"""Calculate the multiplication and sum of two numbers.
+
+Given two integer numbers, write a Python
+code to return their product only if the
+product is equal to or lower than 1000.
+Otherwise, return their sum.
+"""
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+print("The result is " + str(x * y))
