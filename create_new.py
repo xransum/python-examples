60348e78433e265d120f323fa7e4d20c1e210607
xransum <xransum@users.noreply.github.com>
2024-11-16 18:25:48 -0500

    Updated create_new.py with sections.

--- create_new.py
+++ create_new.py
@@ -6,12 +6,36 @@ import re
 from glob import glob
 
 DIR_REGEX = r"^[0-9]+_"
+SECTIONS_REGEX = r"section_[a-z]"
 
 root = os.path.dirname(os.path.abspath(__file__))
-dirs = [d for d in os.listdir(root) if os.path.isdir(d) and re.search(DIR_REGEX, d) is not None]
-arg = input("Input: ").strip()
-new_dir_name = "_".join(re.findall(r"\w+", arg.strip().lower()))
-new_dir_path = os.path.join(root, f"{str(len(dirs) + 1).rjust(2, '0')}_{new_dir_name}")
+
+sections_names = sorted([d for d in os.listdir(root) if re.search(SECTIONS_REGEX, d) is not None])
+
+print("Section select -")
+for i in range(len(sections_names)):
+    section_name = sections_names[i]
+    print(f"{i + 1}. {section_name}")
+
+section_name = sections_names[
+    int(input("Selection: ").strip()) - 1
+]
+section_dir = os.path.join(
+    root,
+    section_name,
+)
+dirs = [
+    d for d in os.listdir(section_dir) if (
+        os.path.isdir(d) and re.search(DIR_REGEX, d) is not None)
+]
+sample_title = input("Title Name: ").strip()
+new_dir_name = "_".join(
+    re.findall(r"\w+", sample_title.strip().lower()),
+)
+new_dir_path = os.path.join(
+    section_dir,
+    f"{str(len(dirs) + 1).rjust(2, '0')}_{new_dir_name}",
+)
 
 for dir_name in dirs:
     name = re.sub(DIR_REGEX, "", dir_name)
@@ -21,7 +45,7 @@ for dir_name in dirs:
 
 print(new_dir_path)
 
-baseline_header = f'''"""{arg}.
+baseline_header = f'''"""{sample_title}.
 
 """
 
