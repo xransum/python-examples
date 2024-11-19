6e85e20c9caa01c7e889798a49f16861b2a76309
xransum <xransum@users.noreply.github.com>
2024-11-19 17:30:35 -0500

    Added Display float number with 2 decimal places using print().

---
+++ section_b/18_display_float_number_with_2_decimal_places_using_print/main.py
@@ -0,0 +1,18 @@
+"""Display float number with 2 decimal places using print().
+
+Given:
+
+```python
+num = 458.541315
+```
+
+Expected Output:
+
+```python
+458.54
+```
+"""
+
+x = float(input("Number: "))
+
+print("%.2f" % x)
