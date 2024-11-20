bdf47ebb4e068145074f508ea5c171aa7d7ddc7d
xransum <xransum@users.noreply.github.com>
2024-11-20 17:46:16 -0500

    Added Display a message "Done" after the successful execution of the for loop.

---
+++ section_b/34_display_a_message_done_after_the_successful_execution_of_the_for_loop/main.py
@@ -0,0 +1,27 @@
+"""Display a message “Done” after the successful execution of the for loop.
+
+For example, the following loop will execute without any error.
+
+Given:
+
+```
+for i in range(5):
+    print(i)
+```
+
+Expected output:
+
+```
+0
+1
+2
+3
+4
+Done!
+```
+"""
+
+for i in range(5):
+    print(i)
+
+print("Done!")
