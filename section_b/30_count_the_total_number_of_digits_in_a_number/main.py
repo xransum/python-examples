5286ee6726612d6cf3d5820147ad1f632a4cbac1
xransum <xransum@users.noreply.github.com>
2024-11-20 17:31:20 -0500

    Added Count the total number of digits in a number.

---
+++ section_b/30_count_the_total_number_of_digits_in_a_number/main.py
@@ -0,0 +1,13 @@
+"""Count the total number of digits in a number.
+
+Write a Python program to count the total
+number of digits in a number using a while
+loop.
+
+For example, the number is 75869, so the
+output should be 5.
+"""
+
+number = int(input("Number: "))
+
+print(len(str(number)))
