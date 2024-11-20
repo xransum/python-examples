dac0fae53ea12c921b4bab386f63edc2f9031d8d
xransum <xransum@users.noreply.github.com>
2024-11-20 17:23:56 -0500

    Added Print multiplication table of a given number.

---
+++ section_b/28_print_multiplication_table_of_a_given_number/main.py
@@ -0,0 +1,28 @@
+"""Print multiplication table of a given number.
+
+Given:
+
+```python
+num = 2
+```
+
+Expected output is:
+
+```
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+```
+"""
+
+num = 2
+
+for x in range(1, 11):
+    print(x*num)
